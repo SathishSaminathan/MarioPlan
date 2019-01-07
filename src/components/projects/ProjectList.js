f0d409fbfd0b8eaf63e21d4139bbf10913594664
SathishSaminathan <Sathishcse1975@gmail.com>
2019-01-07 16:20:36 +0530

    Initial_commit

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,17 @@
 import React from "react";
 
+import ProjectSummery from "./ProjectSummary";
+
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summery">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Sathish Swaminathan</p>
-          <p className="grey-text">3rd September</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summery">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Sathish Swaminathan</p>
-          <p className="grey-text">3rd September</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summery">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Sathish Swaminathan</p>
-          <p className="grey-text">3rd September</p>
-        </div>
-      </div>
+      <ProjectSummery/>
+      <ProjectSummery/>
+      <ProjectSummery/>
+      <ProjectSummery/>
+      <ProjectSummery/>
+      <ProjectSummery/>
+      <ProjectSummery/>
     </div>
   );
 };
