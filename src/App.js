19b6ec4b67a16b4fb59262232797cd53edadb803
SathishSaminathan <Sathishcse1975@gmail.com>
2019-01-07 16:14:50 +0530

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
@@ -9,6 +10,10 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar/>
+          <Switch>
+            <Route exact path="/" component={Dashboard}/>
+            <Route render={()=><h3>Not Found</h3>}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
