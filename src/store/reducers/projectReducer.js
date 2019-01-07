420db2c18c1d182356f06a77f584c589253d38b3
SathishSaminathan <Sathishcse1975@gmail.com>
2019-01-07 18:18:26 +0530

    initial_commit

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState={}
+const initState={
+    projects:[
+       {id:'1', title:'help me to find', content:'blah blah blah'}, 
+       {id:'2', title:'collect the all stars', content:'blah blah blah'},
+       {id:'3', title:'hunt egg with yoshi', content:'blah blah blah'},
+    ]
+}
 
 const projectReducer=(state = initState, action)=>{
     return state;
